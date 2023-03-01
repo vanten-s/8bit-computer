29c4ffd4986e81a3059bd6cf85dc97dcbd96a812
vanten-s <svante.spannar@gmail.com>
2023-03-01 20:11:08 +0100

    Assembler now supports labels letsgoo

--- assembler/assemble.py
+++ assembler/assemble.py
@@ -31,6 +31,45 @@ IN_COMMANDS = {
         "F-IN":          0x2,
 }
 
+labels = { }
+
+def parse_label( line: str, line_i: int ):
+    line = line.strip( )
+    if not line.endswith( ":" ): return
+
+    labels[line[ 0:-1 ]] = line_i
+
+    return
+
+def parse( line: str ):
+    # Parse Instruction
+    if ";" in line:
+        line = line[ 0:line.index( ";" ) ]
+
+    words = line.split( )
+    if len( words ) != 3:
+        return b""
+
+    out_command = OUT_COMMANDS[ words[0] ]
+    in_command  = IN_COMMANDS [ words[1] ]
+    try:
+        value       = eval( words[2] )
+
+    except SyntaxError:
+        pass
+
+    if words[2].startswith( "$" ):
+        value = labels[ words[2][1:] ]
+
+    print(value)
+
+    instruction = b""
+    instruction += value.to_bytes                            ( 1, 'big' )
+    instruction += ( out_command + 16 * in_command ).to_bytes( 1, 'big' )
+
+    return instruction
+
+
 if len( sys.argv ) != 2:
     print( "ERROR: provide input file" )
     exit( 1 )
@@ -38,22 +77,35 @@ if len( sys.argv ) != 2:
 with open( sys.argv[1], "r" ) as f:
     lines = f.read( ).strip( ).split( "\n" )
 
+# Preproccesing
+
+i = 0
+while i < len( lines ):
+    lines[i] = lines[i].strip( )
+
+    remove = False
+
+    if ";" in lines[i]:
+        lines[i] = lines[i][ 0 : lines[i].index( ";" ) ]
+
+    if lines[i].startswith( ";" ): remove = True
+    if lines[i] == "":             remove = True
+
+    if lines[i].endswith( ":" ):
+        parse_label( lines[i], i )
+        remove = True
+
+    if remove:
+        lines.remove( lines[i] )
+
+    else:
+        i += 1
+
+print( "\n".join( lines ) )
+
 out = b""
 for line in lines:
-    if ";" in line:
-        line = line[ 0:line.index( ";" ) ]
-
-    words = line.split( )
-    if len( words ) != 3:
-        continue
-
-    out_command = OUT_COMMANDS[ words[0] ]
-    in_command  = IN_COMMANDS [ words[1] ]
-    value       = eval( words[2] )
-
-    instruction = b""
-    instruction += value.to_bytes                            ( 1, 'big' )
-    instruction += ( out_command + 16 * in_command ).to_bytes( 1, 'big' )
+    instruction = parse( line )
     out += instruction
 
 with open( "out.bin", "wb" ) as f:
--- assembler/screen.s
+++ assembler/screen.s
@@ -1,50 +1,53 @@
 
-ROM-OUT FLAGS-IN      0x00    ; 01 Flags
+ROM-OUT FLAGS-IN      0x00    ; Flags
 
-ROM-OUT RAM-ADDR-U-IN 0x00    ; 02 X position
-ROM-OUT RAM-IN        0x00    ; 03
+ROM-OUT RAM-ADDR-U-IN 0x00    ; X position
+ROM-OUT RAM-IN        0x00    
 
-ROM-OUT RAM-ADDR-U-IN 0x01    ; 04 Y position
-ROM-OUT RAM-IN        0x00    ; 05
+ROM-OUT RAM-ADDR-U-IN 0x01    ; Y position
+ROM-OUT RAM-IN        0x00   
 
-ROM-OUT ROM-ADDR-U-IN 0x15    ; 06 Jump to start
-F-OUT   JMP           0xFF    ; 07
+ROM-OUT ROM-ADDR-U-IN $mainloop
+F-OUT   JMP           0xFF    ; Jump to start
 
-ROM-OUT RAM-IN        0x00    ; 08 Store 0 in X position
-ROM-OUT RAM-ADDR-U-IN 0x01    ; 09 Fetch Y position
-RAM-OUT A-IN          0xFF    ; 0A 
-ROM-OUT B-IN          0x01    ; 0B Add 1
-ALU-OUT RAM-IN        0xFF    ; 0C Store in Y position
-ROM-OUT ROM-ADDR-U-IN 0x15    ; 0D 
-F-OUT   JMP           0xFF    ; 0E Jump back
+inc_y:
+ROM-OUT RAM-IN        0x00    ; Store 0 in X position
+ROM-OUT RAM-ADDR-U-IN 0x01    ; Fetch Y position
+RAM-OUT A-IN          0xFF    ; 
+ROM-OUT B-IN          0x01    ; Add 1
+ALU-OUT RAM-IN        0xFF    ; Store in Y position
+ROM-OUT ROM-ADDR-U-IN $mainloop 
+F-OUT   JMP           0xFF    ; Jump back
 
-ROM-OUT RAM-ADDR-U-IN 0x00    ; 0F Fetch X position
-RAM-OUT A-IN          0xFF    ; 10
-ROM-OUT B-IN          0x01    ; 11 Add 1
-ALU-OUT RAM-IN        0xFF    ; 12 store in X position
-ROM-OUT ROM-ADDR-U-IN 0x07    ; 13
-ROM-OUT B-IN          0xC1    ; 14 Add C1 in order to Detect things
-F-OUT   JC            0xFF    ; 15 Jump to Y increment if carry
+inc_x:
+ROM-OUT RAM-ADDR-U-IN 0x00    ; Fetch X position
+RAM-OUT A-IN          0xFF    
+ROM-OUT B-IN          0x01    ; Add 1
+ALU-OUT RAM-IN        0xFF    ; store in X position
+ROM-OUT ROM-ADDR-U-IN $inc_y 
+ROM-OUT B-IN          0xC1    ; Add C1 in order to Detect things
+F-OUT   JC            0xFF    ; Jump to Y increment if carry
 
-ROM-OUT OUTPUT-IN     0x00    ; 16 
-ROM-OUT OUTPUT-IN     0x00+64 ; 17 Select Color
+mainloop:
+ROM-OUT OUTPUT-IN     0x0E    ; Select Color
+ROM-OUT OUTPUT-IN     0x0E+64 
 
-ROM-OUT RAM-ADDR-U-IN 0x01    ; 18 Select Y position
-RAM-OUT OUTPUT-IN     0xFF    ; 19 Output
-RAM-OUT A-IN          0xFF    ; 1A Fetch into A register
-ROM-OUT B-IN          0x40    ; 1B Add 0x40 to clock the shift register
-ALU-OUT OUTPUT-IN     0xFF    ; 1C Output
+ROM-OUT RAM-ADDR-U-IN 0x01    ; Select Y position
+RAM-OUT OUTPUT-IN     0xFF    ; Output
+RAM-OUT A-IN          0xFF    ; Fetch into A register
+ROM-OUT B-IN          0x40    ; Add 0x40 to clock the shift register
+ALU-OUT OUTPUT-IN     0xFF    ; Output
 
-ROM-OUT RAM-ADDR-U-IN 0x00    ; 1D Select Y position
-RAM-OUT OUTPUT-IN     0xFF    ; 1E Output
-RAM-OUT A-IN          0xFF    ; 1F Fetch into A register
-ROM-OUT B-IN          0x40    ; 20 Add 0x40 to clock the shift register
-ALU-OUT OUTPUT-IN     0xFF    ; 21 Output
+ROM-OUT RAM-ADDR-U-IN 0x00    ; Select Y position
+RAM-OUT OUTPUT-IN     0xFF    ; Output
+RAM-OUT A-IN          0xFF    ; Fetch into A register
+ROM-OUT B-IN          0x40    ; Add 0x40 to clock the shift register
+ALU-OUT OUTPUT-IN     0xFF    ; Output
 
-ROM-OUT OUTPUT-IN     0x80    ; 22 
-ROM-OUT OUTPUT-IN     0x00    ; 23 Clock the displays clock
+ROM-OUT OUTPUT-IN     0x80    
+ROM-OUT OUTPUT-IN     0x00    ; Clock the displays clock
 
-ROM-OUT ROM-ADDR-U-IN 0x0E    ; 24
-F-OUT   JMP           0xFF    ; 25
+ROM-OUT ROM-ADDR-U-IN $inc_x  
+F-OUT   JMP           0xFF    ; Jump back to inc_x
 
 
